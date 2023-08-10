31e39143e4fd7702a27abb90dfb2c0742b62f7e9
kmriley17 <kevin17riley@gmail.com>
2023-08-10 09:07:05 -0700

    * Fixed data seed path issue * Added docker-compose

--- Catalog/Catalog.API/Program.cs
+++ Catalog/Catalog.API/Program.cs
@@ -1,66 +1,20 @@
-using Catalog.Core.Repositories;
-using Catalog.Infrastructure.Data;
-using Catalog.Infrastructure.Repositories;
-using HealthChecks.UI.Client;
-using MediatR;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Microsoft.OpenApi.Models;
-using System.Reflection;
+using System.Diagnostics;
+using DnsClient;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace Catalog.API;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddApiVersioning();
-builder.Services.AddHealthChecks()
-    .AddMongoDb(builder.Configuration["DatabaseSettings:ConnectionString"], "Catalog MongoDb Health Checks", HealthStatus.Degraded);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
-    { 
-        c.SwaggerDoc("v1", 
-        new OpenApiInfo()
-        {
-            Title = "Catalog.API",
-            Version = "v1"
-        }); 
-    }
-);
-
-// DI
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
-builder.Services.AddScoped<ICatalogContext, CatalogContext>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IBrandRepository, ProductRepository>();
-builder.Services.AddScoped<ITypesRepository, ProductRepository>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API"));
-    app.UseDeveloperExceptionPage();
-}
-
-app.UseRouting();
-app.UseStaticFiles();
-app.UseAuthorization();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-    endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+    public static void Main(string[] args)
     {
-        Predicate = _ => true,
-        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-    });
-});
+        Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+        CreateHostBuilder(args).Build().Run();
+    }
 
-app.UseHttpsRedirection();
-
-app.Run();
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
